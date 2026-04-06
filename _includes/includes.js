dd835775d65d81f7e2f53fc30f5609bb3fc481d5
PmR76 <705royal@outlook.com>
2026-04-06 02:59:32 +0100

    Update includes.js

--- _includes/includes.js
+++ _includes/includes.js
@@ -1,33 +1,19 @@
 <footer class="site-footer">
   <div class="footer-row">
 
-    <!-- LEFT SIDE -->
     <div class="footer-left">
       © 2026 Crown Creatives<br>
       Crafted with intention, creativity, and resilience.
     </div>
 
-    <!-- CENTER: SOCIAL ICONS -->
     <div class="footer-center">
       <div class="social-group">
-        <a href="https://twitter.com" class="social-icon" aria-label="Twitter">
-          <div class="rune-circle"></div>
-          <img src="/assets/icons/twitter.svg" class="icon" alt="Twitter">
-        </a>
-
-        <a href="https://instagram.com" class="social-icon" aria-label="Instagram">
-          <div class="rune-circle"></div>
-          <img src="/assets/icons/instagram.svg" class="icon" alt="Instagram">
-        </a>
-
-        <a href="https://youtube.com" class="social-icon" aria-label="YouTube">
-          <div class="rune-circle"></div>
-          <img src="/assets/icons/youtube.svg" class="icon" alt="YouTube">
-        </a>
+        <a href="#" class="social-icon">Twitter</a>
+        <a href="#" class="social-icon">Instagram</a>
+        <a href="#" class="social-icon">YouTube</a>
       </div>
     </div>
 
-    <!-- RIGHT SIDE -->
     <div class="footer-right">
       <button class="back-to-top">Back to Top</button>
       <button class="dark-mode-toggle">Dark Mode</button>
@@ -35,4 +21,3 @@
 
   </div>
 </footer>
-
