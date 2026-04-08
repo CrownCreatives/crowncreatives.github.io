17ac9f3f5abf01e9dd8d60ef36c3853c6e969cd9
PmR76 <705royal@outlook.com>
2026-04-08 17:03:16 +0100

    Create backtotop.js

---
+++ assets/js/backtotop.js
@@ -0,0 +1,13 @@
+// Back To Top Button
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("backToTop");
+
+    if (!btn) return;
+
+    btn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
